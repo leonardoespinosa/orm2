4b80996686e9205b8e7b475c6fb38a95f7acb4f3
leonardoespinosa <leonardoespinosa@realbind.com>
2018-05-31 18:07:37 -0500

    Modify step one

--- ormiggaapp/src/pages/contractor/quotation/step-one/step-one.ts
+++ ormiggaapp/src/pages/contractor/quotation/step-one/step-one.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { Media, MediaObject } from '@ionic-native/media';
 import { File } from '@ionic-native/file';
@@ -7,19 +7,34 @@ import { File } from '@ionic-native/file';
     selector: 'page-step-one',
     templateUrl: 'step-one.html'
 })
-export class StepOnePage {
+export class StepOnePage implements OnInit {
 
     private _recording: boolean = false;
     private _filePath: string;
     private _fileName: string;
     private _audio: MediaObject;
     private _audioList: any[] = [];
+    private _recordIndex: number;
 
+    /**
+     * StepOnePage Constructor
+     * @param {NavController} _navCtrl 
+     * @param {Media} _media 
+     * @param {File} _file 
+     * @param {Platform} _platform 
+     */
     constructor(public _navCtrl: NavController,
         private _media: Media,
         private _file: File,
         public _platform: Platform) {
 
+    }
+
+    /**
+     * ngOnInit Implementation
+     */
+    ngOnInit() {
+        this._recordIndex = 0;
     }
 
     /**
@@ -33,12 +48,13 @@ export class StepOnePage {
      * Function to start audio record
      */
     startRecord(): void {
+        this._recordIndex = this._recordIndex + 1;
         if (this._platform.is('ios')) {
-            this._fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.m4a';
+            this._fileName = 'Solicitud_#' + this._recordIndex.toString() + '_' + new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear() + '.m4a';
             this._filePath = this._file.documentsDirectory.replace(/file:\/\//g, '') + this._fileName;
             this._audio = this._media.create(this._filePath);
         } else if (this._platform.is('android')) {
-            this._fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.3gp';
+            this._fileName = 'Solicitud_#' + this._recordIndex.toString() + '_' + new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear() + '.3gp';
             this._filePath = this._file.externalDataDirectory.replace(/file:\/\//g, '') + this._fileName;
             this._audio = this._media.create(this._filePath);
         }
