7b54c09f5a140b8bf82c6d15a0d28d0bb7e2a8ba
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-11 15:55:09 -0500

    Add Environment variables in docker file

--- express-server/config.js
+++ express-server/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     rethinkdb: {
-        host: "localhost",
-        port: 28015,
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 28015,
         authKey: "",
-        db: "ormigga"
+        db: process.env.DB_NAME || "ormigga"
     },
     secret: process.env.FLIGO_SECRET || 'fl!g02017@' // never use default
 }
